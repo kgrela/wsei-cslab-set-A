5a565d24c41de6b437257f38cf250d1dab507526
Kacper Grela <kacper.grela@microsoft.wsei.edu.pl>
2021-11-17 18:49:48 +0100

    Added task A11

---
+++ Task_A11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_A11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            char[] tab = new char[n];
+            
+            for(int i=0; i<n; i++)
+            {
+                Console.Write("Enter a character with index " + i + ": ");
+                char.TryParse(Console.ReadLine(), out tab[i]);
+            }
+
+            foreach(char x in tab)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
